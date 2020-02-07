da8613b9f29e7a98878d7248cc095bc5a4747398
hansa-lallu <hansalallu@gmail.com>
2020-02-07 16:49:05 +0000

    implemented switching turns

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -27,4 +27,10 @@ describe Game do
       expect(game.player_2).to eq player_2
     end 
   end 
+
+  describe '#current_turn' do
+    it 'starts as player 1' do 
+      expect(game.current_turn).to eq player_1
+    end
+  end
 end
