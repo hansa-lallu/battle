092fb3601eca18fefe2d4872303820f454be40e5
hansa-lallu <hansalallu@gmail.com>
2020-03-26 16:04:20 +0000

    fixed syntax error

--- spec/features/hit_point_spec.rb
+++ spec/features/hit_point_spec.rb
@@ -3,4 +3,9 @@ feature 'hit_point' do
     sign_in_and_play
     expect(page).to have_content "David: 60 HP"
   end
+ 
+  scenario 'see Player 1 hit points' do
+    sign_in_and_play
+    expect(page).to have_content 'John: 60HP'
+  end
 end
